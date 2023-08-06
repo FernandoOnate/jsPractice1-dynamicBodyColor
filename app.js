eb206151eb18fcc1a6c3d079eaa1a35b6325126a
Fernando Oñate Rodriguez <ofertar.fernando@gmail.com>
2023-08-06 18:28:19 -0500

    Primer commit, v1.0.0

---
+++ app.js
@@ -0,0 +1,18 @@
+// const changeColorButton = document.getElementById('boton-color');
+// const colorParagraph = document.querySelector('#color');
+// const digitos = '0123456789ABCDEF';
+// const generateRandomColor = () => {
+//     let colorHex = '#';
+
+//     for(let i = 0; i < 6; i++){
+//         let randomIndex = Math.floor(Math.random() * 16);
+//         colorHex += digitos[randomIndex];
+//     }
+
+//     return colorHex;
+// }
+// changeColorButton.addEventListener('click', function(){
+//     let randomColor = generateRandomColor();  
+//     colorParagraph.textContent = randomColor;
+//     document.body.style.backgroundColor = randomColor;
+// });
